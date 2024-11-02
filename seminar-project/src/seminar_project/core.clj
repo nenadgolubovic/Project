5cf4be57f59e1d33211670977fe14cb56abeb265
nenadgolubovic <nenad.golubovic999@gmail.com>
2024-11-02 18:43:10 +0100

    Initial Commit

--- seminar-project/src/seminar_project/core.clj
+++ seminar-project/src/seminar_project/core.clj
@@ -1,6 +1,14 @@
-(ns seminar-project.core)
+(ns seminar-project.core
+       (:require [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def http-call "https://api.schiphol.nl/public-flights/flights?includedelays=false&page=0&sort=%2BscheduleTime")
+(defn api-shipol
+  []
+  (let [url (client/get http-call)
+        headers {"Accept" "application/json"
+                 "app_id" "82405ed0"
+                 "app_key" "9d7ffea2adf25591b3b527d5a61b761d"
+                 "ResourceVersion" "v4"
+                 }]))
+
+(print (api-shipol))
