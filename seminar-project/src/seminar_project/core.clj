e20f0aeb18d9a2f780dc5c88a65b644f9177329b
nenadgolubovic <nenad.golubovic999@gmail.com>
2024-11-08 19:25:58 +0100

    Api request

--- seminar-project/src/seminar_project/core.clj
+++ seminar-project/src/seminar_project/core.clj
@@ -1,14 +1,14 @@
 (ns seminar-project.core
-       (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]))
 
 (def http-call "https://api.schiphol.nl/public-flights/flights?includedelays=false&page=0&sort=%2BscheduleTime")
-(defn api-shipol
+
+(def params {:Accept "application/json"
+             :app_id "82405ed0"
+             :app_key "9d7ffea2adf25591b3b527d5a61b761d"
+             :ResourceVersion "v4"})
+
+(defn api-schiphol
   []
-  (let [url (client/get http-call)
-        headers {"Accept" "application/json"
-                 "app_id" "82405ed0"
-                 "app_key" "9d7ffea2adf25591b3b527d5a61b761d"
-                 "ResourceVersion" "v4"
-                 }]))
-
-
+  (let [response (client/get http-call {:headers params})]
+    response))
